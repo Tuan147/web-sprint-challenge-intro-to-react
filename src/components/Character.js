1a4759959f026b03225731ea291cdfbe5c867d7b
Tuan147 <90153951+Tuan147@users.noreply.github.com>
2021-12-10 12:27:07 -0500

    added my character component moving to app now then styling after

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,27 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+
+const CharStyle = styled.div`
+
+`
+
+
+const Character = props => {
+    const {data} = props;
+
+
+    return (
+        <CharStyle>
+            <div className="container">
+                <h2>Name: {data.name}</h2>
+                <p>Height: {data.height}</p>
+                <p>Mass: {data.mass}</p>
+                <p>Gender: {data.gender}</p>
+            </div>
+        </CharStyle>
+    )
+}
+
+export default Character;
